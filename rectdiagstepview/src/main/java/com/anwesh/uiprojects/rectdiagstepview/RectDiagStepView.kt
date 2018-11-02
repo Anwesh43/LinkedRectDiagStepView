2c83b6cf651228dbe3967c1fe233ecfefd6cea34
Anwesh43 <anweshthecool0@gmail.com>
2018-11-02 11:23:01 +0530

    adding libraries and defining nodes

--- rectdiagstepview/src/main/java/com/anwesh/uiprojects/rectdiagstepview/RectDiagStepView.kt
+++ rectdiagstepview/src/main/java/com/anwesh/uiprojects/rectdiagstepview/RectDiagStepView.kt
@@ -3,3 +3,12 @@ package com.anwesh.uiprojects.rectdiagstepview
 /**
  * Created by anweshmishra on 02/11/18.
  */
+
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+
+val nodes : Int = 5
