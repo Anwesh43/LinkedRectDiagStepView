bf65ab1205462a29b5b0f0bed3414adfda389dbd
Anwesh43 <anweshthecool0@gmail.com>
2018-11-02 11:37:40 +0530

    adding RectDiagStepView class

--- rectdiagstepview/src/main/java/com/anwesh/uiprojects/rectdiagstepview/RectDiagStepView.kt
+++ rectdiagstepview/src/main/java/com/anwesh/uiprojects/rectdiagstepview/RectDiagStepView.kt
@@ -41,3 +41,21 @@ fun Canvas.drawRDSNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class RectDiagStepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
